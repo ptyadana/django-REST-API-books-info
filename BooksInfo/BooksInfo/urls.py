6973c82210d72a82adc7fd6b3acb3e1a998a8efe
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-03-19 11:00:45 +0800

    fix serializer and add Views, urls

--- BooksInfo/BooksInfo/urls.py
+++ BooksInfo/BooksInfo/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from books_info_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/',views.booklist.as_view()),
 ]
--- BooksInfo/books_info_app/serializers.py
+++ BooksInfo/books_info_app/serializers.py
@@ -1,9 +1,8 @@
 from rest_framework import serializers
 from books_info_app.models import Book
 
-class BookSerializer(serializers.Serializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        #same as 
-        # fields = '__all__'
-        fields = [id,book_name,author_name,book_price,book_quantity]
+        #same as fields = '__all__'
+        fields = ['id', 'book_name', 'author_name', 'book_price', 'book_quantity']
